376378f718622c20c614aeca0acbf74746508e53
Siva Krishna <sivakrishna@uky.edu>
2018-01-31 04:56:41 -0500

    update data with random data

--- module-02/app1.js
+++ module-02/app1.js
@@ -4,16 +4,30 @@
 //
 // You can use the data to change the outcome of the bar chart. Most commonly used to manipulate the height.
 
-var data = [ 10,20,30,40,50 ]
+// var data = [ 10,20,30,40,50 ]
+
+// Generating random data can help battle test your graph as it allows you to cover most scenarios.
+//
+// There are situations where clients may not be able to provide you data so you'll be responsible for generating dummy data in the mean time.
+//
+// The D3 random library takes care of generating random data for you.
+
+// generating random data
+var data = [];
+
+for (i = 0; i < 20; i++) {
+  // Math.round()
+  var num = Math.floor(Math.random() * 50);
+  data.push(num);
+}
 
 var el = d3.select("#chart")
   .selectAll('div')
   .data(data)
   .enter()
   .append('div')
-  .attr("class","bar") //classed('bar', true)
-  .style("height", function(d){
+  .attr("class", "bar") //classed('bar', true)
+  .style("height", function(d) {
     var height = d * 3;
-    return height+"px";
-  })
-  ;
+    return height + "px";
+  });
