ed94e224400434070bb34e84e63ad8026980a1c9
Siva Krishna <sivakrishna@uky.edu>
2018-02-10 17:26:21 -0500

    added remove button with bar removal functionality
    lot of changes to code. it's kind of little tricker as data from array changes to array of objects

--- module-03/app1.js
+++ module-03/app1.js
@@ -8,7 +8,71 @@
 
 // see below for info about updating the bar chart
 
-var data = [6, 20, 14, 24, 3, 7, 17, 23, 2, 6, 25, 11, 26, 9, 12]
+var data = [{
+    key: 0,
+    num: 6
+  },
+  {
+    key: 1,
+    num: 20
+  },
+  {
+    key: 2,
+    num: 14
+  },
+  {
+    key: 3,
+    num: 24
+  },
+  {
+    key: 4,
+    num: 3
+  },
+  {
+    key: 5,
+    num: 7
+  },
+  {
+    key: 6,
+    num: 17
+  },
+  {
+    key: 7,
+    num: 23
+  },
+  {
+    key: 8,
+    num: 2
+  },
+  {
+    key: 9,
+    num: 6
+  },
+  {
+    key: 10,
+    num: 25
+  },
+  {
+    key: 11,
+    num: 11
+  },
+  {
+    key: 12,
+    num: 26
+  },
+  {
+    key: 13,
+    num: 9
+  },
+  {
+    key: 14,
+    num: 12
+  }
+];
+
+var key = function(d){
+  return d.key;
+}
 
 // Create SVG element
 var chart_width = 800;
@@ -29,39 +93,41 @@ var x_scale = d3.scaleBand()
   .paddingInner(0.05);
 
 var y_scale = d3.scaleLinear()
-  .domain([0, d3.max(data)])
+  .domain([0, d3.max(data, function(d){
+    return d.num;
+  })])
   .range([0, chart_height]);
 
 // Bind data and create bars
 svg.selectAll('rect')
-  .data(data)
+  .data(data, key)
   .enter()
   .append('rect')
-  .attr("x", function(d, i) {
+  .attr("x", function(d,i) {
     return x_scale(i); // 0 - 0, 1 - 30, 2 - 60
   })
   .attr("y", function(d) {
-    return chart_height - y_scale(d);
+    return chart_height - y_scale(d.num);
   })
   .attr("width", x_scale.bandwidth())
   .attr("height", function(d) {
-    return y_scale(d);
+    return y_scale(d.num);
   })
   .attr('fill', '#7ED26D');
 
 // create labels
 svg.selectAll('text')
-  .data(data)
+  .data(data, key)
   .enter()
   .append('text')
   .text(function(d) {
-    return d;
+    return d.num;
   })
   .attr("x", function(d, i) {
     return x_scale(i) + (x_scale.bandwidth()) / 2;
   })
   .attr("y", function(d) {
-    return chart_height - y_scale(d) + 15;
+    return chart_height - y_scale(d.num) + 15;
   })
   .attr('font-size', '15px')
   .attr('fill', '#fff')
@@ -94,40 +160,42 @@ svg.selectAll('text')
 d3.select('.update').on('click', function() {
   // console.log('Om Namah Shivaya!');
   //data.reverse();
-  data[0] = 50;
-  y_scale.domain([0, d3.max(data)]); // updating the domain
+  data[0].num = 50;
+  y_scale.domain([0, d3.max(data, function(d){
+    return d.num;
+  })]); // updating the domain
 
   svg.selectAll('rect')
-    .data(data)
+    .data(data, key)
     .transition()
-    .delay(function(d,i){
+    .delay(function(d, i) {
       return i / data.length * 1000;
     })
     .duration(1000)
     .ease(d3.easeCubicInOut)
     .attr("y", function(d) {
-      return chart_height - y_scale(d);
+      return chart_height - y_scale(d.num);
     })
     .attr("height", function(d) {
-      return y_scale(d);
+      return y_scale(d.num);
     });
   // create labels
   svg.selectAll('text')
-    .data(data)
+    .data(data, key)
     .transition()
-    .delay(function(d,i){
+    .delay(function(d, i) {
       return i / data.length * 1000;
     })
     .duration(1000) // defaul 250 -- 1/4 of a second
     .ease(d3.easeCubicInOut)
     .text(function(d) {
-      return d;
+      return d.num;
     })
     .attr("x", function(d, i) {
       return x_scale(i) + (x_scale.bandwidth()) / 2;
     })
     .attr("y", function(d) {
-      return chart_height - y_scale(d) + 15;
+      return chart_height - y_scale(d.num) + 15;
     })
 
 });
@@ -143,23 +211,27 @@ d3.select('.update').on('click', function() {
 // Add Data
 d3.select('.add').on('click', function() {
   // Add new data
-  var new_num = Math.floor(Math.random() * d3.max(data));
-  data.push(new_num);
+  var new_num = Math.floor(Math.random() * d3.max(data, function(d){
+    return d.num;
+  }));
+  data.push({
+    key: data[data.length-1].key+1, num: new_num
+  });
 
   // Update Scales
   x_scale.domain(d3.range(data.length));
-  y_scale.domain([0, d3.max(data, function(d){
-    return d;
+  y_scale.domain([0, d3.max(data, function(d) {
+    return d.num;
   })]);
 
   // Select Bars
   var bars = svg.selectAll('rect')
-    .data(data);
+    .data(data, key);
 
   // Add new bar
   bars.enter()
     .append('rect')
-    .attr('x', function(d,i){
+    .attr('x', function(d, i) {
       return x_scale(i);
     })
     .attr('y', chart_height)
@@ -173,37 +245,105 @@ d3.select('.add').on('click', function() {
       return x_scale(i); // 0 - 0, 1 - 30, 2 - 60
     })
     .attr("y", function(d) {
-      return chart_height - y_scale(d);
+      return chart_height - y_scale(d.num);
     })
     .attr("width", x_scale.bandwidth())
     .attr("height", function(d) {
-      return y_scale(d);
+      return y_scale(d.num);
     })
     .attr('fill', '#7ED26D');
 
-    // Create Labels
-    var labels = svg.selectAll('text')
-      .data(data);
+  // Create Labels
+  var labels = svg.selectAll('text')
+    .data(data, key);
 
-    labels.enter()
-      .append('text')
-      .text(function(d) {
-        return d;
-      })
-      .attr("x", function(d, i) {
-        return x_scale(i) + (x_scale.bandwidth()) / 2;
-      })
-      .attr("y", chart_height)
-      .attr('font-size', '15px')
-      .attr('fill', '#fff')
-      .attr('text-anchor', 'middle')
-      .merge(labels)
-      .transition()
-      .duration(1000)
-      .attr("x", function(d, i) {
-        return x_scale(i) + (x_scale.bandwidth()) / 2;
-      })
-      .attr("y", function(d) {
-        return chart_height - y_scale(d) + 15;
-      });
+  labels.enter()
+    .append('text')
+    .text(function(d) {
+      return d.num;
+    })
+    .attr("x", function(d, i) {
+      return x_scale(i) + (x_scale.bandwidth()) / 2;
+    })
+    .attr("y", chart_height)
+    .attr('font-size', '15px')
+    .attr('fill', '#fff')
+    .attr('text-anchor', 'middle')
+    .merge(labels)
+    .transition()
+    .duration(1000)
+    .attr("x", function(d, i) {
+      return x_scale(i) + (x_scale.bandwidth()) / 2;
+    })
+    .attr("y", function(d) {
+      return chart_height - y_scale(d.num) + 15;
+    });
+});
+
+// D3 has an exit mode which will store references to elements that have no data bound to them. You can call the exit() function loop through these elements.
+//
+// If you apply the remove() function to a transition, then the function will wait until the transition is finished.
+//
+// Data joins are the process of binding a value in your data to an element. It's the connection between data in your array and elements on the page.
+//
+// By default, D3 will use index ordering which means the first value in the array is bound to the first element on the page. So on and so forth.
+
+// Remove Data
+d3.select('.remove').on('click', function() {
+  // Remove first item
+  data.shift();
+
+  // Update Scales
+  x_scale.domain(d3.range(data.length));
+  y_scale.domain([0, d3.max(data, function(d){
+    return d.num;
+  })]);
+
+  // Select Bars
+  var bars = d3.selectAll('rect').data(data, key);
+
+  // Update Bars
+  bars.transition()
+    .duration(500)
+    .attr("x", function(d, i) {
+      return x_scale(i); // 0 - 0, 1 - 30, 2 - 60
+    })
+    .attr("y", function(d) {
+      return chart_height - y_scale(d.num);
+    })
+    .attr("width", x_scale.bandwidth())
+    .attr("height", function(d) {
+      return y_scale(d.num);
+    })
+    .attr('fill', '#7ED26D');
+
+  // Remove Bars
+  bars.exit()
+    .transition()
+    .attr('x', -x_scale.bandwidth())
+    .remove();
+
+  // Select Labels
+  var labels = d3.selectAll('text').data(data, key);
+
+  // Update Labels
+  labels.transition()
+    .duration(500)
+    .attr('text-anchor', 'start')
+    .attr("x", function(d, i) {
+      return x_scale(i) + (x_scale.bandwidth()) / 2;
+    })
+    .attr("y", function(d) {
+      return chart_height - y_scale(d.num) + 15;
+    });
+
+  // Remove labels
+  labels.exit()
+    .transition()
+    .attr('x', -x_scale.bandwidth())
+    .remove();
+
+
+
+
 });
